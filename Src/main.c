e46f5bb262ff8cf76164747648a0d23f31e67877
automatiek <arjan.kamphuis@gmail.com>
2023-09-19 10:10:20 +0200

    Template project working

--- Src/main.c
+++ Src/main.c
@@ -19,7 +19,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "edge-impulse-sdk/porting/ei_classifier_porting.h"
+//#include "edge-impulse-sdk/porting/ei_classifier_porting.h"
 #include <stdlib.h>
 
 /** @addtogroup STM32F4xx_HAL_Examples
@@ -71,7 +71,6 @@ int main(void)
        - Low Level Initialization
      */
   HAL_Init();
-  //HAL_SetTickFreq(1000);
 
   /* Configure the system clock to 180 MHz */
   SystemClock_Config();
@@ -105,14 +104,10 @@ int main(void)
   
   BSP_LED_On(LED2);
 
-  /* Output a message on Hyperterminal using printf function */
-  printf("\n\r UART Printf Example: retarget the C library printf function to the UART\n\r");
-  printf("** Test finished successfully. ** \n\r");
+  printf("** ST system init finished successfully. ** \n\r");
 
-    ei_printf("Test if this works\r\n");
-  ei_main();
+  //ei_main();
     
-  ei_printf("TIck freq: %d\r\n", HAL_GetTickFreq());
   
   /* Infinite loop */
   while (1)
